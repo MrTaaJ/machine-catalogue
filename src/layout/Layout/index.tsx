7e7fa1ecb7d1b06bf206daffd026438ecc0c145a
SAHEED TAJUDEEN <saheed.taaj@gmail.com>
2023-07-31 01:36:15 +0100

    Feat: Implement Layout

--- src/layout/Layout/index.tsx
+++ src/layout/Layout/index.tsx
@@ -4,10 +4,12 @@ import SideBar from "../SideBar";
 const Layout = ({ children }: LayoutSchema) => {
   return (
     <main className="w-full h-screen flex">
-      <div className="w-1/6 min-w-[200px] border border-gray-500 p-4 bg-gray-100">
+      <div className="w-1/6 min-w-[200px] border border-gray-500 p-4 bg-gray-100 h-screen ">
         <SideBar />
       </div>
-      <div className="w-5/6 px-4 h-full bg-gray-200">{children}</div>
+      <div className="w-5/6 px-4 bg-gray-200 h-screen overflow-y-scroll overflow-x-hidden">
+        {children}
+      </div>
     </main>
   );
 };
